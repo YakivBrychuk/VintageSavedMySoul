6c7a65056871f9aa12f1e1a1ee19ea781b30fd87
superuser <jaycobneros@protonmail.com>
2024-09-19 00:26:21 +0100

    Added home app and templates

---
+++ home/urls.py
@@ -0,0 +1,8 @@
+# Description: This file contains the URL patterns for the home app.
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='home')
+]
