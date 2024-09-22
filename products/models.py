ebb9b447935cefb6347a04a7cfa67606129b533a
superuser <jaycobneros@protonmail.com>
2024-09-22 19:44:18 +0100

    Customized admin

--- products/models.py
+++ products/models.py
@@ -9,6 +9,9 @@ from PIL import Image
 # Create your models here.
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
