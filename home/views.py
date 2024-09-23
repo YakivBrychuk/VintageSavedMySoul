53fee891dd394cecf49276d56627166a54af2a89
superuser <jaycobneros@protonmail.com>
2024-09-24 00:02:01 +0100

    Added model for hero images and registered model in admin.py

--- home/views.py
+++ home/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from .models import HeroImage
 # Create your views here.
 
 def index(request):
     """ A view to display the index page """
     
     return render(request, 'home/index.html')
+
+def home(request):
+    hero_images = HeroImage.objects.filter(is_active=True)
+    return render(request, 'home/index.html', {'hero_images': hero_images})
