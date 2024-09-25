df0efd21a35aea1eca0c320b86dd114ff1ff4702
superuser <jaycobneros@protonmail.com>
2024-09-25 13:03:34 +0100

    Add bag context & delivery logic

--- vintagesavedmysoul/settings.py
+++ vintagesavedmysoul/settings.py
@@ -76,6 +76,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required for django-allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -153,8 +154,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+FREE_DELIVERY_THRESHOLD = 160
+STANDARD_DELIVERY_PERCENTAGE = 10       
