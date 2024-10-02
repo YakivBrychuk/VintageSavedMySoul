3dc810c4643185031c9d8408de8694cda7ae5ca6
superuser <jaycobneros@protonmail.com>
2024-10-02 16:10:12 +0100

    Install stripe, set currency, export public & secret keys

--- checkout/views.py
+++ checkout/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
+
+import stripe
 
 def checkout(request):
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
+
+    current_bag = bag_contents(request)
+    total_bag = current_bag['total_bag']
+    stripe_total = round(total_bag * 100)
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
