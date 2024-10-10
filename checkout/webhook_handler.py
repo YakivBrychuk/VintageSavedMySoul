08a2ede37abd41793a69077161fff600ff8a8c2b
superuser <jaycobneros@protonmail.com>
2024-10-10 21:35:58 +0100

    Updated checkout form#

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -43,17 +43,17 @@ class StripeWH_Handler:
         attempt = 1
         while attempt <= 5:
             try:
-                order = order.objects.get (
-                    full_name__iexact=billing_details.name,
-                    email__iexact=billing_details.email,
-                    phone_number__iexact=billing_details.phone,
-                    country__iexact=billing_details.address.country,
-                    postcode__iexact=billing_details.address.postal_code,
-                    town_or_city__iexact=billing_details.address.city,
-                    street_address1__iexact=billing_details.address.line1,
-                    street_address2__iexact=billing_details.address.line2,
-                    county__iexact=billing_details.address.state,
-                    grand_total=grand_total,
+                order = Order.objects.create(
+                    full_name=shipping_details.name,
+                    user_profile=profile,
+                    email=billing_details.email,
+                    phone_number=shipping_details.phone,
+                    country=shipping_details.address.country,
+                    postcode=shipping_details.address.postal_code,
+                    town_or_city=shipping_details.address.city,
+                    street_address1=shipping_details.address.line1,
+                    street_address2=shipping_details.address.line2,
+                    county=shipping_details.address.state,
                     original_bag=bag,
                     stripe_pid=pid, 
                 )
