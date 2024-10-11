22b6c19e1b768279e05fd392d76ba6fde67c9ff3
superuser <jaycobneros@protonmail.com>
2024-10-11 13:42:06 +0100

    Add profiles app

---
+++ profiles/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here
+
+def profile(request):
+    """ Display the user's profile. """
+    template = 'profiles/profile.html'
+    context = {
+        'on_profile_page': True
+    }
+
+    return render(request, template, context)
+
