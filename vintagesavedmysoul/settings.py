89f248d0ad0868cc980242743d50af17289c5d53
superuser <jaycobneros@protonmail.com>
2024-10-12 22:25:44 +0100

    Complete confirmation emails

--- vintagesavedmysoul/settings.py
+++ vintagesavedmysoul/settings.py
@@ -174,3 +174,4 @@ STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_KEY', '')
+DEFAULT_FROM_EMAIL = 'vintagesavedmysoul@example.com'
