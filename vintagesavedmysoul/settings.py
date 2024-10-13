46b664eb87dbe2cc26b80362d1890ca69077db4b
superuser <jaycobneros@protonmail.com>
2024-10-13 11:32:31 +0100

    Removed secret key and set debug

--- vintagesavedmysoul/settings.py
+++ vintagesavedmysoul/settings.py
@@ -27,7 +27,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production
 DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['vintagesavedmysoul-eb73f1fba3d0.herokuapp.com', 'localhost', '127.0.0.1']
